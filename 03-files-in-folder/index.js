1a8ed1fb0a8d7db2dbbd761c5e9be46ead5ecb00
Kirill-Fokin <kirill19011998@mail.ru>
2024-01-22 18:41:25 +0300

    feat: add 03 task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,17 @@
+const fs = require('fs/promises');
+const path = require('path');
+const folderPath = path.join(__dirname, 'secret-folder');
+const { stdout } = process;
+
+fs.readdir(folderPath, { withFileTypes: true }).then((files) => {
+  files.forEach((file) => {
+    let filePath = path.join(folderPath, file.name);
+    let ext = path.extname(filePath);
+    fs.stat(filePath).then((stat) => {
+      const fileSize = `${stat.size / 1024}`;
+      stdout.write(
+        `${path.basename(filePath, ext)} - ${ext} - ${fileSize}kb\n`,
+      );
+    });
+  });
+});
