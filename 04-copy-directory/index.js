d1fa62d19053e46999fa3ac8ef6d5f615caeafcc
Kirill-Fokin <kirill19011998@mail.ru>
2024-01-23 01:07:14 +0300

    feat: add 04 task

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,34 @@
+const fs = require('fs/promises');
+const fss = require('fs');
+const path = require('path');
+const folderPath = path.join(__dirname, 'files');
+const { stdout } = process;
+
+fs.mkdir(path.join(__dirname, 'files-copy'), true)
+  .then(function () {
+    console.log('Directory created successfully');
+  })
+
+  .then(function () {
+    fs.readdir(folderPath, { withFileTypes: true }).then((files) => {
+      files.forEach((file) => {
+        let filePath = path.join(folderPath, file.name);
+        let ext = path.extname(filePath);
+        fss.copyFile(
+          path.join(folderPath, file.name),
+          path.join(__dirname, 'files-copy', file.name),
+          (err) => {
+            if (err) throw err;
+            console.log('Файл успешно скопирован');
+          },
+        );
+      });
+    });
+  })
+  .catch(function (err) {
+    if (err.code === 'EEXIST') {
+      console.log('folder already exist');
+    } else {
+      console.log('somethin went wrong');
+    }
+  });
