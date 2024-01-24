296ad45a09b33ad63240bb770d80011b1f10b4c0
Kirill-Fokin <kirill19011998@mail.ru>
2024-01-24 10:10:16 +0300

    fix: fix 04 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -34,6 +34,22 @@ fs.mkdir(path.join(__dirname, 'files-copy'), true )
   })
   .catch(function (err) {
     if (err.code === 'EEXIST') {
+      fs.readdir(folderPath, { withFileTypes: true }).then((files) => {
+        files.forEach((file) => {
+          let filePath = path.join(folderPath, file.name);
+          let ext = path.extname(filePath);
+          fss.copyFile(
+            path.join(folderPath, file.name),
+            path.join(__dirname, 'files-copy', file.name),
+            (err) => {
+              if (err) throw err;
+              console.log('Файл успешно скопирован');
+            },
+          );
+        });
+      })
+      
+      
     } else {
       console.log('somethin went wrong');
     }
