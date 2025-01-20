93d6993bde3c003628723c12342b3b01d6fb4308
Kirill Fokin <kirill19011998@mail.ru>
2025-01-20 21:41:21 +0300

    refactor: code rafactoring

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,20 +1,23 @@
-const fs = require("fs");
+const fs = require('fs');
 const path = require('path');
-const writeStream = fs.createWriteStream('05-merge-styles/project-dist/bundle.css');
+const writeStream = fs.createWriteStream(
+  '05-merge-styles/project-dist/bundle.css',
+);
 const slylesPath = '05-merge-styles/styles';
 
 fs.readdir(slylesPath, { withFileTypes: true }, (err, files) => {
   if (err) {
-    process.stdout.write("Something went wrong: " + err);
+    process.stdout.write('Something went wrong: ' + err);
   } else {
-    files.filter((file) => path.extname(file.name) === '.css')
-         .forEach( (file) => {
-           const filePath = path.join(slylesPath, file.name);
-           const readStream = fs.createReadStream(filePath);
-           readStream.on('data', text => {
-             writeStream.write(`${text}\n`);
-           });
-         });
-    process.stdout.write("bundle is created !");
+    files
+      .filter((file) => path.extname(file.name) === '.css')
+      .forEach((file) => {
+        const filePath = path.join(slylesPath, file.name);
+        const readStream = fs.createReadStream(filePath);
+        readStream.on('data', (text) => {
+          writeStream.write(`${text}\n`);
+        });
+      });
+    process.stdout.write('bundle is created !');
   }
 });
